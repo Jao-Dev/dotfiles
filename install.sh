91363cad6f41f258eebb94e635f7f466d7dc8d08
João Paulo <144500373+Jao-Dev@users.noreply.github.com>
2026-05-06 19:16:12 -0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -8,5 +8,9 @@ ln -sf ~/dotfiles/nvim ~/.config/nvim
 ln -sf ~/dotfiles/waybar ~/.config/waybar
 ln -sf ~/dotfiles/wallpapers ~/.config/wallpapers
 ln -sf ~/dotfiles/ulauncher ~/.config/ulauncher
+ls -sf ~/dotfiles/fastfetch ~/.config/fastetch
+ls -sf ~/dotfiles/qutebrowser ~/.config/qutebrowser
+ls -sf ~/dotfiles/superfile ~/.config/superfile
+
 
 echo "Pronto!"
